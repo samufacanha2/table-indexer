a304c323d30e41926d98deb3779593c71d8abfab
Samuel Façanha <samufacanha@gmail.com>
2024-03-21 18:15:49 -0300

    feat: head n table lines

--- main.py
+++ main.py
@@ -5,47 +5,72 @@ import time
 
 
 class WordSearchGUI:
+
     def __init__(self, master):
         self.master = master
         master.title("Word Search")
 
+        row = 0
+
+        # head
+        self.label_head = tk.Label(master, text="Head Lines:")
+        self.label_head.grid(row=row, column=0, sticky="e")
+
+        self.head_size = tk.Entry(master)
+        self.head_size.grid(row=row, column=1)
+        self.head_size.insert(0, "5")
+
+        self.head_button = tk.Button(master, text="Head", command=self.head)
+        self.head_button.grid(row=row, column=2, sticky="w")
+        self.head_button["state"] = "disabled"
+
+        row += 1
+
+        # Input Section
         self.label_word = tk.Label(master, text="Word:")
-        self.label_word.pack()
+        self.label_word.grid(row=row, column=0, sticky="e")
 
         self.entry_word = tk.Entry(master)
-        self.entry_word.pack()
-        self.entry_word.insert(0, "hello")  # Default value for word
+        self.entry_word.grid(row=row, column=1)
+        self.entry_word.insert(0, "hello")
+
+        self.search_button = tk.Button(master, text="Search", command=self.find_word)
+        self.search_button.grid(row=row, column=2, sticky="w")
+        self.search_button["state"] = "disabled"
+
+        row += 1
 
         self.label_page_size = tk.Label(master, text="Page Size:")
-        self.label_page_size.pack()
+        self.label_page_size.grid(row=row, column=0, sticky="e")
 
         self.entry_page_size = tk.Entry(master)
-        self.entry_page_size.pack()
-        self.entry_page_size.insert(0, "1000")  # Default value for page_size
+        self.entry_page_size.grid(row=row, column=1)
+        self.entry_page_size.insert(0, "1000")
+
+        row += 1
 
         self.label_bucket_size = tk.Label(master, text="Bucket Size:")
-        self.label_bucket_size.pack()
+        self.label_bucket_size.grid(row=row, column=0, sticky="e")
 
         self.entry_bucket_size = tk.Entry(master)
-        self.entry_bucket_size.pack()
-        self.entry_bucket_size.insert(0, "905")  # Default value for bucket_size
+        self.entry_bucket_size.grid(row=row, column=1)
+        self.entry_bucket_size.insert(0, "905")
 
+        row += 1
+
+        # Action Buttons Section
         self.generate_table_button = tk.Button(
             master,
             text="Generate Table Structures",
             command=self.generate_table_structures,
         )
-        self.generate_table_button.pack()
+        self.generate_table_button.grid(row=row, column=0, columnspan=3)
 
-        self.search_button = tk.Button(master, text="Search", command=self.find_word)
-        self.search_button.pack()
-        self.search_button["state"] = "disabled"  # Initially disable the search button
+        row += 1
 
+        # Results Section
         self.result_area = scrolledtext.ScrolledText(master, height=10)
-        self.result_area.pack()
-
-        self.table_instance = None
-        self.hash_table_instance = None
+        self.result_area.grid(row=row, column=0, columnspan=3)
 
     def generate_table_structures(self):
         self.result_area.delete("1.0", tk.END)
@@ -81,9 +106,9 @@ class WordSearchGUI:
             tk.END,
             f"Overflows: {self.hash_table_instance.get_overflows()} ({(self.hash_table_instance.get_overflows() * 100)/ self.hash_table_instance.get_bucket_count():.5f}%)",
         )
-        self.search_button["state"] = (
-            "normal"  # Enable the search button after structures are generated
-        )
+        self.search_button["state"] = "normal"
+
+        self.head_button["state"] = "normal"
 
     def find_word(self):
         self.result_area.delete("1.0", tk.END)
@@ -132,6 +157,26 @@ class WordSearchGUI:
                     return [tuple, time.time() - start_time, memory_access_count]
 
         return [None, time.time() - start_time, memory_access_count]
+
+    def head(self):
+        self.result_area.delete("1.0", tk.END)
+        head_size = int(self.head_size.get())
+
+        if self.table_instance is None or self.hash_table_instance is None:
+            self.result_area.insert(
+                tk.END, "Please generate table structures before searching.\n"
+            )
+            return
+
+        line_count = 0
+        for page in self.table_instance._pages:
+            for tuple in page.get_page():
+                if line_count == head_size:
+                    return
+                self.result_area.insert(
+                    tk.END, f"{line_count + 1} - {tuple.get_value()}\n"
+                )
+                line_count += 1
 
 
 if __name__ == "__main__":
